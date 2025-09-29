227924e5c8a92d648f63f78598e55746dfa1d889
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-09-30 01:29:25 +0600

    All desgin Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { FaArrowRight } from "react-icons/fa";
+import {
+    FaArrowRight,
+    FaFacebook,
+    FaInstagram,
+    FaLinkedin,
+    FaLocationArrow,
+    FaPhone,
+    FaTwitter,
+    FaVoicemail,
+} from "react-icons/fa";
 import "./App.css";
+import { FaLocationPin } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 function App() {
     return (
@@ -81,6 +92,55 @@ function App() {
                             Book a Professional Plumber <FaArrowRight />
                         </button>
                     </div>
+                </div>
+            </section>
+
+            <section className="bg-[#FFFFFF] lg:p-[120px] sm:p-[40px] p-[20px]">
+                <footer className="grid lg:grid-cols-3 sm:grid-cols-1 sm:gap-10 lg:gap-0 gap-10 text-base-content p-10 lg:w-[1360px] mx-auto">
+                    <aside className="">
+                        <h1 className="text-[32px]">Business Logo</h1>
+                        <div className="flex gap-4 mt-[28px]">
+                            <FaFacebook size={25} />
+                            <FaLinkedin size={25} />
+                            <FaTwitter size={25} />
+                            <FaInstagram size={25} />
+                        </div>
+                    </aside>
+                    <div className="grid lg:grid-cols-3 sm:grid-cols-2 sm:col-span-2 gap-10 sm:gap-0 lg:gap-0">
+                        <nav className="flex flex-col gap-4">
+                            <h6 className="footer-title">Services</h6>
+                            <a className="link link-hover">Branding</a>
+                            <a className="link link-hover">Design</a>
+                            <a className="link link-hover">Marketing</a>
+                            <a className="link link-hover">Advertisement</a>
+                        </nav>
+                        <nav className="flex flex-col gap-4">
+                            <h6 className="footer-title">Company</h6>
+                            <a className="link link-hover">About us</a>
+                            <a className="link link-hover">Contact</a>
+                            <a className="link link-hover">Jobs</a>
+                            <a className="link link-hover">Press kit</a>
+                        </nav>
+                        <nav className="flex flex-col gap-4 sm:col-span-2 lg:col-span-1 sm:mt-10 lg:mt-0">
+                            <h6 className="footer-title">Contact Info</h6>
+                            <a className="link link-hover flex gap-2 items-center">
+                                <FaLocationPin /> 1 Sail Street, London, SE11
+                                6NQ
+                            </a>
+                            <a className="link link-hover flex gap-2 items-center">
+                                {" "}
+                                <MdEmail /> enquiries@PlumbingPros.com
+                            </a>
+                            <a className="link link-hover flex gap-2 items-center">
+                                {" "}
+                                <FaPhone /> 020 4527 6474
+                            </a>
+                        </nav>
+                    </div>
+                </footer>
+                <div className="flex justify-between lg:w-[1360px] mx-auto mt-[80px] ">
+                    <p className="text-[#2c2e2f]">© Plumbing Pros. All Rights Reserved</p>
+                    <p className="text-[#313334]">Website by IH Adventure And Creative</p>
                 </div>
             </section>
         </div>
